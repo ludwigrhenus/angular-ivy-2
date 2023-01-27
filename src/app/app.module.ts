90faa836f95fd4259c435e5fdeca60ffb865f6b0
ludwigrhenus <123156163+ludwigrhenus@users.noreply.github.com>
2023-01-27 12:26:45 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [CommonModule, FormsModule, BrowserModule],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+  bootstrap: [AppComponent, HeroesComponent, HeroDetailComponent],
+})
+export class AppModule {}
