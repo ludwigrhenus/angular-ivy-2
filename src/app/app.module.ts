7bd3adb35a7a7c42ab9e36282ed84b466b1b628c
ludwigrhenus <123156163+ludwigrhenus@users.noreply.github.com>
2023-02-10 15:03:24 +0100

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  imports: [FormsModule, BrowserModule],
+  imports: [FormsModule, BrowserModule, AppRoutingModule],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
